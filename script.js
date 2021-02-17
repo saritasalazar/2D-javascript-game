2990d6635d593cd2b7c738a23f571c49a6caf9e3
Sara <saraesalazar@icloud.com>
2021-02-16 23:02:22 -0500

    create paddle and keyboard controls

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
+
 let ballRadius = 10;
 let x = canvas.width/2;
 let y = canvas.height-30;
 let dx = 2;
 let dy = -2;
+
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width-paddleWidth)/2;
+
+let pressRight = false;
+let pressLeft = false;
+
+document.addEventListener("keydown", handleKeyDown,false);
+document.addEventListener("keyup", handleKeyUp, false);
+
+function handleKeyDown(e) {
+    if(e.key === "Right" || e.key == "ArrowRight"){
+        pressRight = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft"){
+        pressLeft = true;
+    }
+}
+
+function handleKeyUp(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        pressRight = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft"){
+        pressLeft = false;
+    }
+}
 
 function drawBall() {
     ctx.beginPath();
@@ -14,9 +43,18 @@ function drawBall() {
     ctx.closePath();
 }
 
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
     
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
@@ -27,6 +65,19 @@ function draw() {
     
     x += dx;
     y += dy;
+
+    if(pressRight) {
+        paddleX += 7;
+        if (paddleX + paddleWidth > canvas.width){
+            paddleX = canvas.width - paddleWidth;
+        }
+    }
+    else if(pressLeft) {
+        paddleX -= 7;
+        if (paddleX < 0){
+            paddleX = 0;
+        }
+    }
 }
 
 setInterval(draw, 10);
